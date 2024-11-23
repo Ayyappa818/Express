04667b9f32a2178fcefcf64249a37d6a51f4ed11
Ayyappa818 <ayyappa666swamy@gmail.com>
2024-11-23 11:23:44 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,106 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+app.set('view engine','pug')
+var bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.get("/deleteReview/:id",function(req,res){
+    console.log(req.params.id)
+    var reviews = JSON.parse(fs.readFileSync("reviews.txt").toString());
+    reviews.splice(req.params.id,1)
+    fs.writeFileSync('reviews.txt',JSON.stringify(reviews))
+    res.send("delete karo")
+})
+
+app.post("/addReview",function(req,res){
+    console.log(req.body)
+    var reviews = JSON.parse(fs.readFileSync("reviews.txt").toString());
+    reviews.push(req.body)
+    fs.writeFileSync('reviews.txt',JSON.stringify(reviews))
+    res.send("aagu ra aandhi ni thuthara")
+})
+
+app.get("/getReview",function(req,res){
+    res.sendFile(__dirname+"/addreview.html")
+})
+
+app.get('/',function(req,res){
+    var reviews = JSON.parse(fs.readFileSync("reviews.txt").toString());
+    res.render("home",{reviews:reviews,students:['sandeep','philip','sai','tarun']})
+})
+
+app.get("/student",function(req,res){
+    console.log(req.params)
+    res.send("hi student")
+})
+
+app.get("/add/:a/:b",function(req,res){
+    var x = +req.params.a;
+    var y = +req.params.b;
+    var z = x+y;
+    console.log(req.params)
+    res.send(z.toString())
+})
+
+app.get("/studentdetails",function(req,res){
+    console.log(req.params) 
+    res.sendFile(__dirname+'/data.html')
+})
+
+app.get("/data",function(req,res){
+    res.send(students)
+})
+
+var students = require("./students")
+app.get("/studentsdet",function(req,res){
+    var tb="";
+    for(var i=0;i<=students.length-1;i++){
+        tb=tb+`<tr>
+            <td>${students[i].firstname}</td>
+            <td>${students[i].lastname}</td>
+            <td>${students[i].age}</td>
+            <td>${students[i].gender}</td>
+            <td><a href="/students/${students[i].id}">more</a></td>
+          </tr>`
+    };
+    var table = `<table border='2'>
+    <thead>
+            <tr>
+             <th id="A">Firstname</th>
+             <th>Lastname</th>
+             <th>Age</th>
+             <th>Gender</th>
+             <th>Details</th>
+            </tr>
+          </thead>
+    <tbody>
+      ${tb}
+    </tbody>
+    </table>`
+    res.end(table)
+})
+
+app.get("/students/:fn",function(req,res){
+    var det=null
+    students.map((a,i)=>{
+
+        if(i==req.params.fn){
+            det=a
+        }
+    })
+    res.send(
+       `<div>
+       <ul>
+         <li>Firstname : ${det.firstname}</li>
+         <li>Lastname : ${det.lastname}</li>
+         <li>Gender : ${det.gender}</li>
+         <li>Age : ${det.age}</li>
+         <li>City : ${det.city}</li>
+         <li>PineCode : ${det.pincode}</li>
+       </ul>
+       </div>`    
+    )
+        
+})
+.listen(4500)
---
+++ students.js
@@ -0,0 +1,56 @@
+module.exports = [
+    {
+      "firstname": "John",
+      "lastname": "Doe",
+      "gender": "Male",
+      "age": 20,
+      "city": "New York",
+      "pincode": "10001",
+      "id":1
+    },
+    {
+      "firstname": "Jane",
+      "lastname": "Smith",
+      "gender": "Female",
+      "age": 22,
+      "city": "Los Angeles",
+      "pincode": "90001",
+      "id":2
+    },
+    {
+      "firstname": "Michael",
+      "lastname": "Johnson",
+      "gender": "Male",
+      "age": 19,
+      "city": "Chicago",
+      "pincode": "60601",
+      "id":3
+    },
+    {
+      "firstname": "Emily",
+      "lastname": "Williams",
+      "gender": "Female",
+      "age": 21,
+      "city": "Houston",
+      "pincode": "77001",
+      "id":4
+    },
+    {
+      "firstname": "David",
+      "lastname": "Brown",
+      "gender": "Male",
+      "age": 23,
+      "city": "Phoenix",
+      "pincode": "85001",
+      "id":5
+    },
+    {
+      "firstname": "Sarah",
+      "lastname": "Davis",
+      "gender": "Female",
+      "age": 20,
+      "city": "Philadelphia",
+      "pincode": "19101",
+      "id":6
+    },
+]  
